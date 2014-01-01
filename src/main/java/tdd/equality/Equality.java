9c7fcf65c2966d1dff1bdde42d742cd9c36a6a9d
Yohan Lee <yohanlee@yahoo.com>
2014-01-01 10:51:15 -0500

    Put data in test

--- src/main/java/tdd/equality/Equality.java
+++ src/main/java/tdd/equality/Equality.java
@@ -13,16 +13,4 @@ import tdd.model.Titleist;
  * To change this template use File | Settings | File Templates.
  */
 public class Equality {
-    public static GolfBall[] makeData() {
-        return new GolfBall[]{
-                new GolfBall("Maxfli", "Pro", 100),
-                new GolfBall("Maxfli", "Regular", 90),
-                new GolfBall("Titleist", "Pro V1", 100),
-                new Titleist("Pro V1", 100),
-                new Nike("Pro", 100),
-                new Nike("Amateur", 90),
-                new Callaway("Pro", 100),
-                new Callaway("Amateur", 90)
-        };
-    }
 }
