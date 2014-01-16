33fe42149cb182655023d6bd68be6884e0644bb3
Yohan Lee <yohanlee@yahoo.com>
2014-01-15 21:46:35 -0500

    A Little Java examples

---
+++ src/main/java/littlejava/CartesianPt.java
@@ -0,0 +1,14 @@
+package littlejava;
+
+/**
+ * Created by Yohan on 1/15/14.
+ */
+public class CartesianPt extends PointD {
+    int x;
+    int y;
+
+    public CartesianPt(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ src/main/java/littlejava/PointD.java
@@ -0,0 +1,7 @@
+package littlejava;
+
+/**
+ * Created by Yohan on 1/15/14.
+ */
+public abstract class PointD {
+}
