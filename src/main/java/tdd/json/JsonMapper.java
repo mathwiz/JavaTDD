68991ddb258d4181302586bf9636b40851594e3a
Yohan Lee <yohanlee@yahoo.com>
2014-01-17 20:06:44 -0500

    Jackson example

--- src/main/java/tdd/json/JsonMapper.java
+++ src/main/java/tdd/json/JsonMapper.java
@@ -1,20 +1,25 @@
 package tdd.json;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import tdd.model.GolfBall;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
  * Created by Yohan on 1/16/14.
  */
 public class JsonMapper {
-    public List<GolfBall> deserialize(String json) {
+    public List<GolfBall> deserialize(String json) throws Exception {
         ObjectMapper mapper = new ObjectMapper();
-        return null;
+        return mapper.readValue(json, new TypeReference<GolfBall>(){});
     }
 
-    public String serialize(List<GolfBall> objects) {
-        return null;
+    public String serialize(List<GolfBall> objects) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(objects);
     }
 }
--- src/test/java/tdd/json/JsonMapperTest.java
+++ src/test/java/tdd/json/JsonMapperTest.java
@@ -2,16 +2,33 @@ package tdd.json;
 
 import org.junit.Before;
 import org.junit.Test;
+import tdd.model.Callaway;
+import tdd.model.GolfBall;
+import tdd.model.Nike;
+import tdd.model.Titleist;
 
 import java.io.File;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by Yohan on 1/16/14.
  */
 public class JsonMapperTest {
+    private static List<GolfBall> makeData() {
+        return Arrays.asList(new GolfBall[] {
+                new GolfBall("Maxfli", "Pro", 100),
+                new GolfBall("Maxfli", "Regular", 90),
+                new GolfBall("Titleist", "Pro V1", 100),
+                new Titleist("Pro V1", 100),
+                new Nike("Pro", 100),
+                new Nike("Amateur", 90),
+                new Callaway("Pro", 100),
+                new Callaway("Amateur", 90)
+        });
+    }
+
     String json;
 
     @Before
@@ -23,11 +40,13 @@ public class JsonMapperTest {
 
     @Test
     public void testDeserialize() throws Exception {
-        System.out.println(json);
     }
 
     @Test
     public void testSerialize() throws Exception {
-        System.out.println(json);
+        JsonMapper mapper = new JsonMapper();
+        List<GolfBall> list = makeData();
+        String s = mapper.serialize(list);
+        System.out.println(s);
     }
 }
