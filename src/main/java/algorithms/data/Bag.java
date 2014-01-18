dbe0a1dfeb15f06d2607590f90387192cea3a3ee
mathwiz <yohanlee@yahoo.com>
2014-01-18 14:48:57 -0500

    Refactored data structure hierarchy

--- src/main/java/algorithms/data/Bag.java
+++ src/main/java/algorithms/data/Bag.java
@@ -5,17 +5,7 @@ import java.util.Iterator;
 /**
  * Created by Yohan on 1/17/14.
  */
-public class Bag<E> implements Iterable<E> {
-    private Node<E> first;
-    private int n;
-
-    public boolean isEmpty() {
-        return first == null;
-    }
-
-    public int size() {
-        return n;
-    }
+public class Bag<E> extends LinkedListDataStructure<E> {
 
     public void add(E item) {
         Node<E> oldFirst = first;
@@ -25,26 +15,4 @@ public class Bag<E> implements Iterable<E> {
         n++;
     }
 
-    public Iterator<E> iterator() {
-        return new Iterator<E>() {
-            private Node<E> current = first;
-
-            @Override
-            public boolean hasNext() {
-                return current != null;
-            }
-
-            @Override
-            public E next() {
-                E item = current.getItem();
-                current = current.getNext();
-                return item;
-            }
-
-            @Override
-            public void remove() {
-                throw new UnsupportedOperationException("remove not available");
-            }
-        };
-    }
 }
--- src/main/java/algorithms/data/LinkedListDataStructure.java
+++ src/main/java/algorithms/data/LinkedListDataStructure.java
@@ -1,7 +1,42 @@
 package algorithms.data;
 
+import java.util.Iterator;
+
 /**
  * Created by yohanlee on 1/18/14.
  */
-public abstract class LinkedListDataStructure<E> {
+public abstract class LinkedListDataStructure<E> implements Iterable<E> {
+    protected Node<E> first;
+    protected int n;
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private Node<E> current = first;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public E next() {
+                E item = current.getItem();
+                current = current.getNext();
+                return item;
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException("remove not available");
+            }
+        };
+    }
 }
