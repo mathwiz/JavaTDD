984e9020ed838d1e758c8557576b7dd8c9c91d4e
Yohan Lee <yohanlee@yahoo.com>
2014-01-26 12:49:42 -0500

    Union Find

---
+++ src/main/java/util/Pair.java
@@ -0,0 +1,22 @@
+package util;
+
+/**
+ * Created by Yohan on 1/26/14.
+ */
+public class Pair<E, F> {
+    private E left;
+    private F right;
+
+    public Pair(E left, F right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public E getLeft() {
+        return left;
+    }
+
+    public F getRight() {
+        return right;
+    }
+}
