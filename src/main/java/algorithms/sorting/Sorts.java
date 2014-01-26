eb09360cc2e6bb868bb2ef687f079a0d093b8ef5
Yohan Lee <yohanlee@yahoo.com>
2014-01-26 17:41:40 -0500

    Helpers for Sorting algorithms

--- src/main/java/algorithms/sorting/Sorts.java
+++ src/main/java/algorithms/sorting/Sorts.java
@@ -1,7 +1,39 @@
 package algorithms.sorting;
 
+import java.io.PrintStream;
+
 /**
  * Created by Yohan on 1/26/14.
  */
 public class Sorts {
+    public static void sort(Comparable[] a) {
+        //TODO
+    }
+
+    public static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    public static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    public static void show(Comparable[] a) {
+        show(a, System.out);
+    }
+
+    public static void show(Comparable[] a, PrintStream out) {
+        for (int i = 0; i < a.length; i++)
+            out.print(a[i] + " ");
+        out.println();
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++)
+            if (less(a[i], a[i - 1]))
+                return false;
+        return true;
+    }
 }
