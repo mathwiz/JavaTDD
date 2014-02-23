6637d49a8ae914605d542816bf07d6112a74fc71
mathwiz <mathwiz@gmail.com>
2014-02-23 14:19:50 -0500

    New puzzle

---
+++ src/main/java/puzzlers/Abc.java
@@ -0,0 +1,15 @@
+package puzzlers;
+
+/**
+ * Created by Yohan on 2/23/14.
+ */
+public class Abc {
+    public static void main(String[] args) {
+        String ABC = "ABC";
+        char[] onetwothree = {'1', '2', '3'};
+        System.out.println(ABC + " as easy as " + onetwothree);
+        System.out.println(ABC);
+        System.out.println(onetwothree);
+        System.out.println(ABC + " as easy as " + String.valueOf(onetwothree));
+    }
+}
