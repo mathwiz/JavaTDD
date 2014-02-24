33b2729ace06310426b601118a5d4f07e6bc98fb
mathwiz <mathwiz@gmail.com>
2014-02-23 22:08:16 -0500

    Static partition for general use

---
+++ src/test/java/algorithms/sorting/PartitionTest.java
@@ -0,0 +1,24 @@
+package algorithms.sorting;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Yohan on 1/26/14.
+ */
+public class PartitionTest extends SortTest {
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        Sorts.show(a);
+    }
+
+    @Test
+    public void testPartition() throws Exception {
+        System.out.println("Partition");
+        Assert.assertFalse(Sorts.isSorted(a));
+        Sorts.partition(a, 0, a.length - 1);
+        Sorts.show(a);
+    }
+}
