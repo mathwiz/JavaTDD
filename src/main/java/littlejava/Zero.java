8dba2e4e39ed2fc82c239350ed9496fa0db7287e
mathwiz <mathwiz@gmail.com>
2014-03-16 15:32:15 -0400

    More equals

--- src/main/java/littlejava/Zero.java
+++ src/main/java/littlejava/Zero.java
@@ -13,4 +13,20 @@ public class Zero extends NumD {
     public boolean equals(Object o) {
         return o instanceof Zero;
     }
+
+    public static void main(String[] args) {
+        PieD p =
+                new Top(new SalmonFish(),
+                        new Top(new Integer(42),
+                                new Top(new Zero(),
+                                        new Top(new Long(1),
+                                                new Top(new Zero(),
+                                                        new Top(new Zero(),
+                                                                new Top(new Double(2.3),
+                                                                        new Bot()))))))
+                );
+        System.out.println("Before\n" + p);
+        PieD after = p.rem(new Zero()).rem(new Double(2.3)).rem(new Integer(42)).rem(new SalmonFish());
+        System.out.println("After\n" + after);
+    }
 }
