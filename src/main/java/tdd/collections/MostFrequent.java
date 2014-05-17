8ec1c3e212a5e3d858451c1f11ee2b4f9284b332
mathwiz <mathwiz@gmail.com>
2014-05-16 23:24:20 -0400

    Find the most frequent integer in a list

--- src/main/java/tdd/collections/MostFrequent.java
+++ src/main/java/tdd/collections/MostFrequent.java
@@ -1,11 +1,9 @@
 package tdd.collections;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 /**
+ * Find the most frequent integer in a list. Break ties with smallest value.
  * Created by Yohan on 5/16/2014.
  */
 public class MostFrequent {
@@ -17,8 +15,10 @@ public class MostFrequent {
     }
 
     public static void main(String[] args) {
-        List<Integer> vals = Arrays.asList(2, 3, 1, 2, 1, 2, 3, 3);
-        System.out.println(String.format("Most frequent element is %s", new MostFrequent(vals).find1()));
+        List<Integer> vals = Arrays.asList(-2, -3, -1, -2, -1, -2, -3, -1, -3, -1);
+        MostFrequent mf = new MostFrequent(vals);
+        System.out.println(String.format("Most frequent element is %s", mf.find1()));
+        System.out.println(String.format("Most frequent element is %s", mf.find2()));
     }
 
     protected int find1() {
@@ -34,6 +34,22 @@ public class MostFrequent {
             if (it.getValue() > maxCount) {
                 most = it.getKey();
                 maxCount = it.getValue();
+            }
+        }
+        return most;
+    }
+
+    protected int find2() {
+        int most = Integer.MIN_VALUE;
+        Map<Integer, Integer> counts = new HashMap<>();
+        int maxCount = 0;
+        Collections.sort(nums);
+        for (int it : nums) {
+            Integer count = counts.get(it);
+            counts.put(it, count == null ? 1 : ++count);
+            if (counts.get(it) > maxCount) {
+                maxCount = counts.get(it);
+                most = it;
             }
         }
         return most;
