8c6a13f52c4613653c468933d9f48008ef4bc0f6
mathwiz <mathwiz@gmail.com>
2014-07-12 22:05:52 -0400

    Find the most frequent integer in a list

--- src/main/java/tdd/collections/MostFrequent.java
+++ src/main/java/tdd/collections/MostFrequent.java
@@ -15,10 +15,11 @@ public class MostFrequent {
     }
 
     public static void main(String[] args) {
-        List<Integer> vals = Arrays.asList(-2, -3, -1, -2, -1, -2, -3, -1, -3);
+        List<Integer> vals = Arrays.asList(-2, -3, -1, -2, -1, -2, -3, -1, -3, -3);
         MostFrequent mf = new MostFrequent(vals);
         System.out.println(String.format("Most frequent element is %s", mf.find1()));
         System.out.println(String.format("Most frequent element is %s", mf.find2()));
+        System.out.println(String.format("Most frequent element is %s", mf.find3()));
     }
 
     protected int find1() {
@@ -51,6 +52,28 @@ public class MostFrequent {
                 maxCount = counts.get(it);
                 most = it;
             }
+        }
+        return most;
+    }
+
+    protected int find3() {
+        int most = Integer.MIN_VALUE;
+        int last = Integer.MIN_VALUE;
+        int count = 0;
+        int maxCount = 0;
+        Collections.sort(nums);
+        for (int it : nums) {
+            System.out.println(String.format("%s %s %s %s ", it, count, maxCount, most));
+            if (it == most) {
+                count++;
+                maxCount++;
+            } else if (count == 0) {
+                most = it;
+                count++;
+            } else if (it == last) {
+                count--;
+            }
+            last = it;
         }
         return most;
     }
