72f5ddec84cf239a656cb2080a81caef609f302b
mathwiz <mathwiz@gmail.com>
2014-12-06 20:53:03 -0500

    Package for card games

---
+++ src/main/java/games/Player.java
@@ -0,0 +1,16 @@
+package games;
+
+/**
+ * Created by Yohan on 12/6/2014.
+ */
+public class Player {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
