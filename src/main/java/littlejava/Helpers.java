53b0ad6e9aaebb99ac29bd250e375d5954452ea2
Yohan Lee <yohanlee@yahoo.com>
2024-03-24 17:24:07 -0400

    little

--- src/main/java/littlejava/Helpers.java
+++ src/main/java/littlejava/Helpers.java
@@ -12,4 +12,15 @@ public abstract class Helpers {
     public static void println(Object o) {
         out.println(o);
     }        
+
+    public static String[] parseArg(String arg, String delimiter) {
+        return arg.split(delimiter);
+    }
+
+    public static void main(String[] args) {
+        String[] parsed = parseArg(args[0], ":");
+        for(String elem : parsed) {
+            println(elem);
+        }
+    }
 }
--- src/main/java/littlejava/Main.java
+++ src/main/java/littlejava/Main.java
@@ -5,9 +5,26 @@ import static littlejava.Helpers.*;
 public abstract class Main {
     public static void main(String[] args) {
         try {
-        Class cls = Class.forName(args[0]);
-        println(cls.getName());
-        Object instance = cls.getDeclaredConstructor().newInstance();
+        Class root = Class.forName(args[0]);
+        Object rootObj = root.getDeclaredConstructor().newInstance();
+
+        int params = args.length;
+        for(int i=0; i<params; i++) {
+            println(args[i]);
+        }
+        Object instance = null;
+        if (!"${arg1}".equals(args[1])) {
+            Class next = Class.forName(args[1]);
+            println(next);
+            Class[] types = {  };
+            Object[] parameters = { rootObj };
+            instance = next.getConstructor(types).newInstance(parameters);
+        } else if (!"${arg2}".equals(args[2])) {
+        } else {
+            println("wha?");
+            instance = rootObj;
+        }
+
         println(instance);
         } catch (Exception e) {
             println(e);
