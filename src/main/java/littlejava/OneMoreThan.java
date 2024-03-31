26039ee36cb78154583bf612f84ba565d0c4c42c
Yohan Lee <yohanlee@yahoo.com>
2024-03-31 11:09:11 -0400

    littlejava

--- src/main/java/littlejava/OneMoreThan.java
+++ src/main/java/littlejava/OneMoreThan.java
@@ -1,5 +1,7 @@
 package littlejava;
 
+import static littlejava.Helpers.*;
+
 /**
  * Created by Yohan on 1/15/14.
  */
@@ -27,19 +29,9 @@ public class OneMoreThan extends NumD {
     }
 
     public static void main(String[] args) {
-        PieD p =
-                new Top(new SalmonFish(),
-                        new Top(new Integer(42),
-                                new Top(new OneMoreThan(new Zero()),
-                                        new Top(new Long(1),
-                                                new Top(new Zero(),
-                                                        new Top(new OneMoreThan(new OneMoreThan(new Zero())),
-                                                                new Top(new Double(2.3),
-                                                                        new Bot()))))))
-                );
-        System.out.println("Before\n" + p);
-        PieD after = p.rem(new RemV(), new OneMoreThan(new Zero())).rem(new RemV(), new Zero());
-        System.out.println("After\n" + after);
+        NumD after = new OneMoreThan(new Zero());
+        println("OneMOreThan");
+        println(after);
     }
 
 }
