56192152d8ad45ed0f7d4233f859e86831943b6d
Yohan Lee <yohanlee@yahoo.com>
2024-04-11 11:35:09 -0400

    littlejava

--- src/main/java/littlejava/Tomato.java
+++ src/main/java/littlejava/Tomato.java
@@ -1,5 +1,7 @@
 package littlejava;
 
+import static littlejava.Helpers.*;
+
 /**
  * Created by Yohan on 1/18/14.
  */
@@ -16,5 +18,11 @@ public class Tomato extends ShishD {
     @Override
     boolean onlyOnions() {
         return ooFn.forTomato(this.s);
+    }
+
+    public static void main(String[] args) {
+        ShishD shish = new Tomato(new Onion(new Tomato(new Skewer())));
+        printf("onlyOnions() for %s is: %s\n", shish, shish.onlyOnions());
+        printf("isVegatarian() for %s is: %s\n", shish, shish.isVegetarian());
     }
 }
