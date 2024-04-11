4c9b2c603db8f142649fe72989b3aa5b3c843c8d
Yohan Lee <yohanlee@yahoo.com>
2024-04-11 11:17:02 -0400

    littlejava

--- src/main/java/littlejava/Onion.java
+++ src/main/java/littlejava/Onion.java
@@ -1,5 +1,7 @@
 package littlejava;
 
+import static littlejava.Helpers.*;
+
 /**
  * Created by Yohan on 1/18/14.
  */
@@ -17,5 +19,9 @@ public class Onion extends ShishD {
     @Override
     boolean onlyOnions() {
         return ooFn.forOnion(this.s);
+    }
+
+    public static void main(String[] args) {
+        printf("onlyOnions() is: %s\n", new Onion(new Onion(new Skewer())).onlyOnions());
     }
 }
